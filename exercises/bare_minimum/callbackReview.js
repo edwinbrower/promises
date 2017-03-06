f5c900b74d442bf06c7340c8853a0a39cceb86f2
Edwin Brower <edwin.brower@gmail.com>
2017-03-05 22:42:37 -0800

    Save code from what I looked at earlier

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -5,13 +5,18 @@
 var fs = require('fs');
 var request = require('request');
 
+console.log(request);
+
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, cb) {
+  cb('ENOENT', filePath);
   // TODO
+  // return request.filePath;
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCode = function (url) {
+  // return request.url;
   // TODO
 };
 
