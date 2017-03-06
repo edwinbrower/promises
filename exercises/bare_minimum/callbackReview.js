d09102a3ae4b9d0c38ef1525def452d97d8944af
Edwin Brower <edwin.brower@gmail.com>
2017-03-06 00:45:32 -0800

    Complete callback review and promise constructor. Start promisification.

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -5,19 +5,32 @@
 var fs = require('fs');
 var request = require('request');
 
-console.log(request);
+// console.log(request);
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath, cb) {
-  cb('ENOENT', filePath);
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  fs.readFile(filePath, 'utf8', function(err, data) {
+    // console.log(data);
+    if (err) {
+      callback(err);
+    } else {
+      var first = data.split('\n')[0];
+      callback(null, first);
+    }
+  });
   // return request.filePath;
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // return request.url;
-  // TODO
+var getStatusCode = function (url, callback) {
+  request.get(url, function(err, data) {
+    // console.log(data);
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, data.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
